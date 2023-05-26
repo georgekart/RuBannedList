abc11dbc1484f6cf3b621f83e2d33353bba529fc
georgekart <79908923+georgekart@users.noreply.github.com>
2023-05-26 15:55:06 +0400

    Add files via upload

--- z-i-list.py
+++ z-i-list.py
@@ -12,17 +12,29 @@ file.close()
 length=len(list(data))
 #gets the length of the csv file
 
-column=[None]*length
+domain=[None]*length
+IPs=[None]*length
 for i in range(1,length):
-    column[i]=data[i][1]
+    IPs[i]=data[i][0]
+    domain[i]=data[i][1]
 #creates the list of domain names    
 
-col=list(filter(None,column))
+dom=list(filter(None,domain))
+IPfull=list(filter(None,IPs))
 #removes the empty domain list
+IPconc=" ".join([str(item) for item in IPfull])
+IPspaced=IPconc.replace('|',' ')
+IPsplit=IPspaced.split(' ')
 
-cu=np.unique(col)
+
+du=np.unique(dom)
+iu=np.unique(IPsplit)
 #removes duplicates
 
 with open('domains.txt', 'w') as f:
-    for line in cu:
+    for line in du:
+        f.write(f"{line}\n")
+        
+with open('IPs.txt', 'w') as f:
+    for line in iu:
         f.write(f"{line}\n")
