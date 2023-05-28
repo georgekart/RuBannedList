70123ec43f26e0fcc33e173aa182f1fbfada2b5f
georgekart <79908923+georgekart@users.noreply.github.com>
2023-05-29 00:55:23 +0400

    Update and rename z-i-list-experiment.py to z-i-list.py
    Exports IP and domain lists as well as domain rule in V2Fly format.

--- z-i-list.py
+++ z-i-list.py
@@ -1,11 +1,8 @@
-#import csv
 import numpy as np
 import sys
-import csv
 import pandas as pd
 import requests
 
-#csv.field_size_limit(sys.maxsize)
 url = "https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv"
 
 c=pd.read_csv(url,delimiter=";",encoding='windows-1251',skiprows=1,header=None)
@@ -24,12 +21,11 @@ for i in range(0,length):
 
 dom=list(filter(None,domain))
 IPfull=list(filter(None,IPs))
-#removes the empty lists
+#removes the empty entries
 IPconc=" ".join([str(item) for item in IPfull])
 IPspaced=IPconc.replace('|',' ')
 IPsplit=IPspaced.split(' ')
 #adds IP entries with multiple addresses as an individual entry
-
 
 du=np.unique(dom)
 iu=np.unique(IPsplit)
@@ -42,3 +38,16 @@ with open('domains.txt', 'w') as f:
 with open('IPs.txt', 'w') as f:
     for line in iu:
         f.write(f"{line}\n")
+#Writes domains to domains.txt and IPs to IPs.txt
+
+for i in range(0,len(du)):
+        if(du[i][0]=="*" and du[i][1]=="."):
+            du[i]=du[i][2:]
+        else:
+            du[i]="full:"+du[i]
+            
+with open('category-banned-ru', 'w') as f:
+    for line in du:
+        f.write(f"{line}\n")
+        
+#Writes domain list to domainlist.txt
